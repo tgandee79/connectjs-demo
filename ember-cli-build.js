f4cc849c26e8b298515b15dd2de46d3449dadac7
Todd Gandee <tgandee@bignerdranch.com>
2015-09-29 11:23:33 -0400

    update the structure with Bootstrap and Sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,22 +2,16 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var bootstrapPath = "vendor/bootstrap/";
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'scss'
+      ]
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  // app.import(bootstrapPath + 'js/[module].js');
 
   return app.toTree();
 };
