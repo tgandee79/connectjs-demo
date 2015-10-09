30aba92c1fc5c317041784284db895fd028fedf0
Todd Gandee <tgandee@bignerdranch.com>
2015-10-09 09:22:11 -0400

    add ipsum helper and js vendor file

---
+++ app/helpers/ipsum-text.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export function ispumText(params, hash) {
+  // hash should have range, count, capitalize, structure (p,s,w) for paragraph, sentence, word
+  var lorem = new window.Lorem();
+  var count = 0;
+  var hashrange = hash.range;
+  var loremtype = window.Lorem.TYPE.PARAGRAPH;
+  if (hash.range){
+    var range = hashrange.split("-");
+    count = Math.floor(Math.random() * parseInt(range[1])) + parseInt(range[0]);
+  } else if(hash.count){
+    count = parseInt(hash.count);
+  }
+  if (/s/.test(hash.structure)) {
+    loremtype = window.Lorem.TYPE.SENTENCE;
+  } else if (/w/.test(hash.structure)) {
+    loremtype = window.Lorem.TYPE.WORD;
+  }
+  var text = lorem.createText(count, loremtype);
+  if(hash.capitalize){
+    text = Ember.String.capitalize(text);
+  }
+  return text;
+}
+
+export default Ember.Helper.helper(ispumText);
